c75252a0d76798517baafafc1c69c4154b56d1ac
Nedd Chairi Muñoz <neddchairiweb@gmail.com>
2024-03-16 22:16:28 +0100

    🎉 Adding test for inputs file

--- __test__/inputs.test.ts
+++ __test__/inputs.test.ts
@@ -4,8 +4,31 @@ describe("Inputs files", () => {
   it("Get inputs from Github", () => {
     jest.spyOn(core, "getInput").mockReturnValue("string");
 
-    const res = core.getInput("test1");
-    expect(res).toBe("string");
+    const result: string = core.getInput("test1");
+    expect(result).toBe("string");
+
+    jest.restoreAllMocks();
+  });
+
+  it("Parse input to integer", () => {
+    jest.spyOn(core, "getInput").mockReturnValue("1");
+
+    const result: string = core.getInput("test2");
+    expect(typeof parseInt(result)).toBe("number");
+
+    jest.restoreAllMocks();
+  });
+
+  it("Format the input path file", () => {
+    jest.spyOn(core, "getInput").mockReturnValue("userName/repoName/.github/workflows/test.yaml@refs/heads/development");
+
+    const parts: string[] = core.getInput("test1").split("@");
+
+    const directory: string[] = parts[0].split("/").slice(-3);
+
+    const result: string = directory.join("/");
+
+    expect(result).toEqual(".github/workflows/test.yaml")
 
     jest.restoreAllMocks();
   });
